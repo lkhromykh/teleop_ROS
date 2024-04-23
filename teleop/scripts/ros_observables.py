18072dcab5c18aa5ca9068a629ae40c02833cd7d
lkhromykh <leonidgm@gmail.com>
2024-04-23 16:22:39 +0300

    PointCloud2 transformation via pcl_ros; robotiq url now leads to the fork; CMakeLists.txt removed;

--- teleop/scripts/ros_observables.py
+++ teleop/scripts/ros_observables.py
@@ -7,9 +7,6 @@ import rospy
 import numpy as np
 from ros_numpy.point_cloud2 import pointcloud2_to_xyz_array
 import message_filters as mf
-import tf_conversions
-import tf2_ros
-from tf2_sensor_msgs import do_transform_cloud
 from cv_bridge import CvBridge
 from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import Image, JointState, PointCloud2
@@ -37,26 +34,24 @@ class ROSObservationNode:
         self._subs = ROSObservationNode.Observation(
             image=mf.Subscriber('/rgb/image_raw', Image),
             depth=mf.Subscriber('/depth/image_raw', Image),
-            point_cloud = mf.Subscriber('points2', PointCloud2),
+            point_cloud = mf.Subscriber('points2_transformed', PointCloud2),
             joint_states = mf.Subscriber('joint_states', JointState),
             tcp_frame = mf.Subscriber('tcp_pose', TransformStamped),
             gripper_status=mf.Subscriber('/gripper/status', CModelStatus)
         )
-        self._time_filter = mf.ApproximateTimeSynchronizer(self._subs, 10, 10)
+        self._time_filter = mf.ApproximateTimeSynchronizer(self._subs, 15, .2, allow_headerless=False)
         self._time_filter.registerCallback(self._obs_callback)
-        self._transform = tf2_ros.Buffer().lookup_transform('base', 'rgb_camera_link', rospy.Time())
-        rospy.info('Got transform: %s', self._transform)
 
     def _obs_callback(self, *args, **kwargs) -> None:
+        # rospy.loginfo('callback fired: %f', rospy.Time.now().to_sec())
         with self._lock:
-            rospy.loginfo('callback fired %d', rospy.Time.now())
             self._obs = ROSObservationNode.Observation(*args, **kwargs)
 
     def get_observation(self) -> Dict[str, 'Any']:
+        assert self._obs is not None
         with self._lock:
             obs = copy.deepcopy(self._obs)
-        pcd = do_transform_cloud(obs.point_cloud, self._transform)
-        pcd = pointcloud2_to_xyz_array(pcd, remove_nans=False)
+        pcd = pointcloud2_to_xyz_array(obs.point_cloud, remove_nans=False)
         p, q = (tr := obs.tcp_frame.transform).translation, tr.rotation
         return {
             'image': self._cvbridge.imgmsg_to_cv2(obs.image),
--- tf2_transforms_pkg/src/pcd_transform.cpp
+++ tf2_transforms_pkg/src/pcd_transform.cpp
@@ -1,46 +1,30 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
-#include "message_filters/subscriber.h"
 #include "tf2_ros/transform_listener.h"
-#include "tf2_ros/message_filter.h"
-#include "tf2_sensor_msgs/tf2_sensor_msgs.h"
+#include "pcl_ros/transforms.h"
 
 
 //TODO: remove hardcoded frames, params;
 class TransformedPoints
 {
   public:
-    TransformedPoints()
-    : tf_listener_(tf_buffer_),
-      tf2_filter_(sub_, tf_buffer_, "rgb_camera_link", 10, 0)
+    TransformedPoints() : tf_listener_(tf_buffer_)
     {
-      sub_.subscribe(nh_, "points2", 10);
-      tf2_filter_.registerCallback(&TransformedPoints::points_callback, this);
+      sub_ = nh_.subscribe("points2", 10, &TransformedPoints::points_callback, this);
       pub_ = nh_.advertise<sensor_msgs::PointCloud2>("points2_transformed", 10);
     }
 
   private:
+    ros::NodeHandle nh_;
     tf2_ros::Buffer tf_buffer_;
     tf2_ros::TransformListener tf_listener_;
-    ros::NodeHandle nh_;
-    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_;
-    tf2_ros::MessageFilter<sensor_msgs::PointCloud2> tf2_filter_;
+    ros::Subscriber sub_;
     ros::Publisher pub_;
     sensor_msgs::PointCloud2 transformed_pcd2;
 
     void points_callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
     {
-      try
-      {
-        tf_buffer_.transform<sensor_msgs::PointCloud2>(*msg, transformed_pcd2, "base", ros::Duration(1.));
-      }
-      catch (tf2::TransformException& ex)
-      {
-        ROS_WARN("%s", ex.what());
-        ros::Duration(1.0).sleep();
-        return;
-      }
-      ROS_INFO("Out callback");
+      pcl_ros::transformPointCloud("base", *msg, transformed_pcd2, tf_buffer_);
       pub_.publish(transformed_pcd2);
     }
 
