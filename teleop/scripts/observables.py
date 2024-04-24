ad5c6e6b3f9d23394835b2b35afd197bc2e7b793
lkhromykh <leonidgm@gmail.com>
2024-04-24 19:18:24 +0300

    improving observations: rgb->rgb_to_depth frame, point cloud transformation

--- teleop/scripts/observables.py
+++ teleop/scripts/observables.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, NamedTuple
 import rospy
 import numpy as np
 from scipy.spatial.transform import Rotation
-from ros_numpy.point_cloud2 import pointcloud2_to_xyz_array
+import ros_numpy.point_cloud2 as pc2
 import message_filters as mf
 from cv_bridge import CvBridge
 from geometry_msgs.msg import TransformStamped
@@ -33,14 +33,14 @@ class ROSObservationNode:
         self._cvbridge = CvBridge()
         self._obs = None
         self._subs = ROSObservationNode.Observation(
-            image=mf.Subscriber('/rgb/image_raw', Image),
+            image=mf.Subscriber('/rgb_to_depth/image_raw', Image),
             depth=mf.Subscriber('/depth/image_raw', Image),
             point_cloud = mf.Subscriber('points2_transformed', PointCloud2),
             joint_states = mf.Subscriber('joint_states', JointState),
             tcp_frame = mf.Subscriber('tcp_pose', TransformStamped),
             gripper_status=mf.Subscriber('/gripper/status', CModelStatus)
         )
-        self._time_filter = mf.ApproximateTimeSynchronizer(self._subs, 15, .2, allow_headerless=False)
+        self._time_filter = mf.ApproximateTimeSynchronizer(self._subs, 15, .1, allow_headerless=False)
         self._time_filter.registerCallback(self._obs_callback)
 
     def _obs_callback(self, *args, **kwargs) -> None:
@@ -52,20 +52,28 @@ class ROSObservationNode:
         assert self._obs is not None
         with self._lock:
             obs = copy.deepcopy(self._obs)
-        pcd = pointcloud2_to_xyz_array(obs.point_cloud, remove_nans=False)
         p, q = (tr := obs.tcp_frame.transform).translation, tr.rotation
+        pcd = pc2.pointcloud2_to_array(obs.point_cloud, squeeze=False)
         pos, quat = np.float32([p.x, p.y, p.z]), np.float32([q.x, q.y, q.z, q.w])
         rotvec = Rotation.from_quat(quat).as_rotvec()
         return {
-            'image': self._cvbridge.imgmsg_to_cv2(obs.image),
+            'image': self._cvbridge.imgmsg_to_cv2(obs.image, "rgb8"),
             'depth': self._cvbridge.imgmsg_to_cv2(obs.depth),
-            'point_cloud': np.nan_to_num(pcd, copy=False, dtype=np.float16),
+            'point_cloud': _record_array_to_array(pcd),
             'joint_position': obs.joint_states.position,
             'joint_velocity': obs.joint_states.velocity,
             'tcp_pose': np.r_[pos, rotvec],
             'gripper_pos': obs.gripper_status.gPO / 255.,
             'gripper_is_obj_detected': obs.gripper_status.gOBJ in (2, 3)
         }
+
+def _record_array_to_array(pcd_struct, nan=0., dtype=np.float16):
+    assert pcd_struct.ndim == 2, 'HW format is desired.'
+    pcd = np.zeros(pcd_struct.shape + (3,), dtype=dtype)
+    pcd[..., 0] = pcd_struct['x']
+    pcd[..., 1] = pcd_struct['y']
+    pcd[..., 2] = pcd_struct['z']
+    return np.nan_to_num(pcd, copy=False, nan=nan)
 
 
 if __name__ == '__main__':
