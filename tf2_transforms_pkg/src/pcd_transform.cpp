b2f9a509d972221ea8dfb64b602e38c0c38eeea3
lkhromykh <leonidgm@gmail.com>
2024-08-15 16:31:32 +0300

    tf2_transform_pkg now suitable for remaping

--- tf2_transforms_pkg/src/pcd_transform.cpp
+++ tf2_transforms_pkg/src/pcd_transform.cpp
@@ -1,18 +1,24 @@
-#include <string>
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "tf2_ros/transform_listener.h"
 #include "pcl_ros/transforms.h"
 
+constexpr int QUEUE_SIZE = 2;
 
-//TODO: remove hardcoded frames, params;
 class TransformedPoints
 {
   public:
-    TransformedPoints(std::string frame) : tf_listener_(tf_buffer_)
+    TransformedPoints() : tf_listener_(tf_buffer_)
     {
-      sub_ = nh_.subscribe(frame, 5, &TransformedPoints::points_callback, this);
-      pub_ = nh_.advertise<sensor_msgs::PointCloud2>("points2_transformed", 5);
+      sub_ = nh_.subscribe("points2", QUEUE_SIZE, &TransformedPoints::points_callback, this);
+      pub_ = nh_.advertise<sensor_msgs::PointCloud2>("points2_transformed", QUEUE_SIZE);
+    }
+
+  private:
+    void points_callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+    {
+      if (pcl_ros::transformPointCloud("base", *msg, transformed_pcd2, tf_buffer_))
+        pub_.publish(transformed_pcd2);
     }
 
   private:
@@ -22,19 +28,12 @@ class TransformedPoints
     ros::Subscriber sub_;
     ros::Publisher pub_;
     sensor_msgs::PointCloud2 transformed_pcd2;
-
-    void points_callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
-    {
-      if (pcl_ros::transformPointCloud("base", *msg, transformed_pcd2, tf_buffer_))
-        pub_.publish(transformed_pcd2);
-    }
-
 };
 
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "points2_transformed");
-  TransformedPoints tp("/wrist_camera/depth/color/points");
+  TransformedPoints tp;
   ros::spin();
 }
--- tf2_transforms_pkg/src/tf_to_tcp.cpp
+++ tf2_transforms_pkg/src/tf_to_tcp.cpp
@@ -1,14 +1,21 @@
+#include <string>
 #include "ros/ros.h"
 #include "tf2_ros/transform_listener.h"
 #include "geometry_msgs/TransformStamped.h"
 
-//TODO: This should be done via tf2_ros::MessageFilter
-//  Rate, topic names should be passed as params.
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "tf_to_tcp");
-  ros::NodeHandle node;
-  ros::Publisher pub = node.advertise<geometry_msgs::TransformStamped>("tcp_pose", 10);
+  ros::NodeHandle node("~");
+  std::string frame;
+  while (!node.getParam("frame", frame))
+  {
+    ROS_ERROR("Can't fetch frame param.");
+    ros::Duration(1.0).sleep();
+  }
+  ROS_WARN("Got frame name: %s", frame.c_str());
+
+  ros::Publisher pub = node.advertise<geometry_msgs::TransformStamped>("transformed_frame", 10);
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
 
@@ -17,16 +24,13 @@ int main(int argc, char **argv)
   while (node.ok())
   {
     try 
-    {
-      transform = tfBuffer.lookupTransform("base", "tool0_controller", ros::Time(0));
-    }
+      transform = tfBuffer.lookupTransform("base", frame, ros::Time(0));
     catch (tf2::TransformException &ex)
     {
       ROS_WARN("%s", ex.what());
       ros::Duration(1.0).sleep();
       continue;
     }
-
     pub.publish(transform);
     rate.sleep();
   }
